65db0e1e38b7cf7f3bb2f341458c63b7046905c6
foto1864 <fotinakisgiannis23@gmail.com>
2023-07-04 01:48:42 +0300

    have i forgotten how to code?

--- birthday_notifications/modules/UsingADTMap_BirthdayNotifications/bd_notifs.c
+++ birthday_notifications/modules/UsingADTMap_BirthdayNotifications/bd_notifs.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include "ADTMap.h"
 
 typedef void* Pointer;
 
@@ -12,6 +14,62 @@ typedef struct {
     int hours;          // Hours
     int minutes;        // Minutes
 } date_info;
+
+int compare_dates(DateInfo info_1, DateInfo info_2){
+    int month_1;
+}
+
+Map load_birthdays(void) {
+    Map birthdays = map_create(compare_dates, free, free);
+
+    DateInfo info_JK = malloc(sizeof(*info_JK));
+    info_JK->date = 13;
+    info_JK->month = 3;
+    info_JK->year = 2004;
+    map_insert(birthdays, info_JK, "Jim Kontopidis");
+
+    DateInfo info_AS = malloc(sizeof(*info_AS));
+    info_AS->date = 25;
+    info_AS->month = 2;
+    info_AS->year = 2004;
+    map_insert(birthdays, info_AS, "Anastasia Spaidioti");
+
+    DateInfo info_GF = malloc(sizeof(*info_GF));
+    info_GF->date = 6;
+    info_GF->month = 1;
+    info_GF->year = 2004;
+    map_insert(birthdays, info_GF, "Giannis Foteinakis");
+
+    DateInfo info_DF = malloc(sizeof(*info_DF));
+    info_DF->date = 19;
+    info_DF->month = 4;
+    info_DF->year = 2005;
+    map_insert(birthdays, info_DF, "Dimitris Foteinakis");
+
+    DateInfo info_AF = malloc(sizeof(*info_AF));
+    info_AF->date = 24;
+    info_AF->month = 10;
+    info_AF->year = 1966;
+    map_insert(birthdays, info_AF, "Andreas Foteinakis");
+
+    DateInfo info_ET = malloc(sizeof(*info_ET));
+    info_ET->date = 23;
+    info_ET->month = 10;
+    info_ET->year = 1968;
+    map_insert(birthdays, info_ET, "Eva Tasiou");
+
+    DateInfo info_test = malloc(sizeof(*info_test));
+    info_test->date = 4;
+    info_test->month = 7;
+    info_test->year = 2023;
+    map_insert(birthdays, info_test, "TEST_DATE");
+
+    return birthdays;
+}
+
+void unload_birthdays(Map birthdays) {
+    map_destroy(birthdays);
+}
 
 // Pointer to the struct date_info
 typedef date_info * DateInfo;
@@ -60,14 +118,22 @@ void print_birthday_info(BirthdayInfo bd_info) {
 
 int main(void) {
 
+    // Get and print the information of current day
     DateInfo date_info = get_date_info();
     print_date_info(date_info);
-
-    BirthdayInfo bd_info = get_birthday_info(date_info);
-    print_birthday_info(bd_info);
-
     free(date_info);
-    free(bd_info);
+
+    // Test
+    Map birthdays = load_birthdays();
+    unload_birthdays(birthdays);
+    free(birthdays);
+
+    //BirthdayInfo bd_info = get_birthday_info(date_info);
+    //print_birthday_info(bd_info);
+    //free(bd_info);
+
+    
+
 
     return 0;
 }
