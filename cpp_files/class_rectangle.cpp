f7b9cfb3eaf2602d2fb63af54564cfbecf6be892
foto1864 <fotinakisgiannis23@gmail.com>
2023-10-30 19:04:34 +0200

    tf how does c++ work??

--- cpp_files/class_rectangle.cpp
+++ cpp_files/class_rectangle.cpp
@@ -32,5 +32,23 @@ class Rectangle {
 
 int main(void) {
 
+    Rectangle *recArray = new Rectangle[3];
+    
+    Rectangle *rec_01 = new Rectangle;
+    rec_01->setHeight(5);
+    rec_01->setWidth(7);
+    
+    Rectangle *rec_02 = new Rectangle;
+    rec_02->setHeight(6);
+    rec_02->setWidth(8);
+
+    recArray[0] = *rec_01;
+    recArray[1] = *rec_02;
+
+    for (int i=0; i<2; i++) {
+        cout << "Area of rectangle " << i << " is " << recArray[i].getArea() << endl;
+        cout << "Perimeter of rectangle " << i << " is " << recArray[i].getPerimeter() << endl;
+    }
+
     return 0;
 }
