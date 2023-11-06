451b1f59e0ef17985e025ac49cfa9dddf6ba3427
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-06 19:05:22 +0200

    return values

--- labs_dit_3rd_sem/lab_03/lab_03_01.cpp
+++ labs_dit_3rd_sem/lab_03/lab_03_01.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-
 class Point {
     private:
-        float x;        // x-coordinate
-        float y;        // y-coordinate
+        float x; // x-coordinate
+        float y; // y-coordinate
         void printInfo(void);
     public:
         void set(const float &px, const float &py);
@@ -14,9 +13,9 @@ class Point {
 
 class Triangle {
     private:
-        Point a;        // Point 1
-        Point b;        // Point 2
-        Point c;        // Point 3
+        Point a; // Point 1
+        Point b; // Point 2
+        Point c; // Point 3
     public:
         void set(const Point &pa, const Point &pb, const Point &pc);
         void print(void);
@@ -26,23 +25,27 @@ class Triangle {
 
 void Point::printInfo(void) {
     cout << "(" << x << "," << y << ")" << endl;
+    return;
 }
 
 void Point::print(void) {
     printInfo();
+    return;
 }
 
 void Point::set(const float &px, const float &py) {
-    x = px;     // Set x-coordinate
-    y = py;     // Set y-coordinate
+    x = px; // Set x-coordinate
+    y = py; // Set y-coordinate
+    return;
 }
 
 ////////////////////////// Functions of class Triangle //////////////////////////
 
 void Triangle::set(const Point &pa, const Point &pb, const Point &pc) {
-    a = pa;     // Set coordinates for Point A.
-    b = pb;     // Set coordinates for Point B.
-    c = pc;     // Set coordinates for Point C.
+    a = pa; // Set coordinates for Point A.
+    b = pb; // Set coordinates for Point B.
+    c = pc; // Set coordinates for Point C.
+    return;
 }
 
 void Triangle::print(void) {
@@ -50,6 +53,7 @@ void Triangle::print(void) {
     a.print();
     b.print();
     c.print();
+    return;
 }
 
 
