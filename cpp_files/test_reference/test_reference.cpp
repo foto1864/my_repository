234043b96a22843aff4088903f4b4fa97c08332c
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-06 17:52:14 +0200

    Trying to make sense of that pass by reference shi

---
+++ cpp_files/test_reference/test_reference.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int multiply(int &a, int &b);
+int multiplySimple(int a, int b);
+
+int main() {
+
+    int a = 5, b = 10;
+    int res1 = multiply(a, b);
+    int res2 = multiplySimple(a, b);
+    cout << "Using Pass by Reference Result is " << res1 << endl;
+    cout << "Using Pass by Value Result is " << res2 << endl;
+    if (res1 == res2) 
+        cout << "As you can see they are exactly the same lol!" << endl;
+
+    return 0;
+}
+
+int multiply(int &a, int &b) {
+    return a*b;
+}
+
+int multiplySimple(int a, int b) {
+    return a*b;
+}
