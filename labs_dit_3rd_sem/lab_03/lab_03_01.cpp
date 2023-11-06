198e0cfdc1db6d2484a0e146d56cd883f7caf73a
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-06 18:47:08 +0200

    edo kanoume na poume lab_03

--- labs_dit_3rd_sem/lab_03/lab_03_01.cpp
+++ labs_dit_3rd_sem/lab_03/lab_03_01.cpp
@@ -4,28 +4,67 @@ using namespace std;
 
 class Point {
     private:
-        float x;
-        float y;
+        float x;        // x-coordinate
+        float y;        // y-coordinate
+        void printInfo(void);
     public:
-        void set(float px, float py) {
-            x = px;
-            y = py;
-        }
+        void set(const float &px, const float &py);
+        void print(void);
 };
 
 class Triangle {
     private:
-        Point a;
-        Point b;
-        Point c;
+        Point a;        // Point 1
+        Point b;        // Point 2
+        Point c;        // Point 3
     public:
-        void set(Point &pa, Point &pb, Point &pc) {
-            a = pa;
-            b = pb;
-            c = pc;
-        }
+        void set(const Point &pa, const Point &pb, const Point &pc);
+        void print(void);
 };
+
+////////////////////////// Functions of class Point //////////////////////////
+
+void Point::printInfo(void) {
+    cout << "(" << x << "," << y << ")" << endl;
+}
+
+void Point::print(void) {
+    printInfo();
+}
+
+void Point::set(const float &px, const float &py) {
+    x = px;     // Set x-coordinate
+    y = py;     // Set y-coordinate
+}
+
+////////////////////////// Functions of class Triangle //////////////////////////
+
+void Triangle::set(const Point &pa, const Point &pb, const Point &pc) {
+    a = pa;     // Set coordinates for Point A.
+    b = pb;     // Set coordinates for Point B.
+    c = pc;     // Set coordinates for Point C.
+}
+
+void Triangle::print(void) {
+    cout << "The triangle is made out of the following three points: " << endl;
+    a.print();
+    b.print();
+    c.print();
+}
+
 
 int main(void) {
 
+    Point a, b, c;
+    a.set(2, 3);
+    b.set(3, 6);
+    c.set(4, 9);
+
+    Triangle tr_01;
+    tr_01.set(a, b, c);
+
+    cout << endl;
+    tr_01.print();
+
+    return 0;
 }
