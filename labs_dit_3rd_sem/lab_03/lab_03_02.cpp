7e008f3bc55f0bffbca915b953f3ca122e5e1f16
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-06 20:34:18 +0200

    get pet ke prasina aloga navm

--- labs_dit_3rd_sem/lab_03/lab_03_02.cpp
+++ labs_dit_3rd_sem/lab_03/lab_03_02.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 using namespace std;
 
+class Person {
+    private:
+        string name;
+        Pet *pet;
+    public:
+        void setName(string n);
+        void getPet(void);
+};
+
+class Pet {
+    private:
+        string name;
+        Person *owner;
+    public:
+        void setName(string n);
+        void setOwner(Person *newOwner);
+};
+
+////////////////////////// Functions of class Person //////////////////////////
+void Person::getPet(void) {
+    Pet *pet = new Pet;
+    pet->setOwner(this);
+}
+void Person::setName(string n) {
+    name = n;
+    return;
+}
+
+////////////////////////// Functions of class Pet //////////////////////////
+void Pet::setOwner(Person *newOwner) {
+    owner = newOwner;
+    return;
+}
+void Pet::setName(string n) {
+    name = n;
+    return;
+}
+
 int main(void) {
+
+    Person Giannis;
+    Giannis.setName("Giannis");
+    Pet Noisette;
+    Noisette.setName("Noisette");
+    Noisette.setOwner(&Giannis);
+    Giannis.getPet();
+
+
     return 0;
 }
