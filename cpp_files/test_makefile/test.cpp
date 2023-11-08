6614819fb4626a7defde860f53ef241e0d160cde
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-08 16:32:58 +0200

    ipame oxi ola mazi

--- cpp_files/test_makefile/test.cpp
+++ cpp_files/test_makefile/test.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
+#include "test.h"
 using namespace std;
+
+///////////////////////////// Functions of Class Card ///////////////////////////////
+
+void Card::setValue(int v) {
+    value = v;
+}
+void Card::setSuit(Suit s) {
+    suit = s;
+}
+int Card::getSuit(void) {
+    return suit;
+}
+int Card::getValue(void) {
+    return value;
+}
+Card::Card(): value(0), suit(NoTrump) {}
+Card::Card(int v, Suit s): value(v), suit(s) {}
+
+////////////////////////// Functions of Class BridgeHand //////////////////////////// 
+
+void BridgeHand::setHand(Card *array) {
+    hand = new Card[13];
+    for (int i=0; i<13; i++) {
+        hand[i] = array[i];
+    }
+}
+void BridgeHand::setTotalPoints(Card *array) {
+    int hcp = 0; 
+    for (int i=0; i<13; i++) {
+        hcp += array[i].getValue();
+    }
+    totalPoints = hcp;
+}
+int BridgeHand::getTotalPoints(void) {
+    return totalPoints;
+}
+void BridgeHand::printHand(void) {
+    for (int i=0; i<13; i++) {
+        cout << hand[i].getValue() << " ";
+    }
+    cout << endl;
+}
 
 int main(void) {
     
+    Card c1(1, 1);
+    Card c2(13, 1);
+    Card c3
+
     cout << "Testing to see if I can make Makefiles work in C++" << endl;
     cout << "Also testing to see how header files work" << endl;
     return 0;
--- cpp_files/test_makefile/test.h
+++ cpp_files/test_makefile/test.h
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-typedef enum Suit { 1 = Clubs; 2 = Diamonds; 3 = Hearts; 4 = Spades};
-typedef enum Figure { 11 = Jack; 12 = Queen; 13 = King};
+typedef enum Suit { Clubs = 1, Diamonds = 2, Hearts = 3, Spades = 4, NoTrump = 5};
+//typedef enum Figure { Jack = 11, Queen = 12, King = 13, Ace = 1};
 
 class Card {
     private:
         int value;
         Suit suit;
-
+    public:
+        void setValue(int v);
+        void setSuit(Suit s);
+        int getValue(void);
+        int getSuit(void);
+        Card();
+        Card(int v, Suit s);
 };
 
 class BridgeHand {
-
+    private:
+        Card *hand;
+        int totalPoints;
+    public:
+        void setHand(Card *array);
+        void setTotalPoints(Card *array);
+        int getTotalPoints(void);
+        void printHand(void);
 };
