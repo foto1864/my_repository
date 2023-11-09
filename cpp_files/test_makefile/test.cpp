11c0dce18ff2b1c1b501fe6f58da67989310636f
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-09 16:51:37 +0200

    tested friend functions

--- cpp_files/test_makefile/test.cpp
+++ cpp_files/test_makefile/test.cpp
@@ -7,10 +7,17 @@ class Rectangle {
         double height;
     public:
         static int count;
+        // Default Constructor
+        Rectangle() {
+            cout << "Rec Created." << endl;
+            count++;
+        }
+        // Constructor Initializer List
         Rectangle(double w, double h): width(w), height(h) {
             cout << "Rec Created. " << endl;
             count++;
         }
+        // Destructor 
         ~Rectangle() {
             cout << "Rec Destroyed. " << endl;
             count--;
@@ -18,9 +25,11 @@ class Rectangle {
         friend Rectangle addRectangle(const Rectangle &r1, const Rectangle &r2);
 };
 
-Rectangle addRectangle(const Rectangle &r1, const rectangle &r2) {
+Rectangle addRectangle(const Rectangle &r1, const Rectangle &r2) {
     Rectangle newRect;
-    newRect.
+    newRect.height = r1.height+r2.height;
+    newRect.width = r2.width+r2.width;
+    return newRect;
 }
 
 int Rectangle::count = 0;
@@ -29,7 +38,8 @@ int main(void) {
     
     Rectangle r1 = Rectangle(3, 4);
     Rectangle r2 = Rectangle(5, 6);
-
+    cout << "In total there are " << Rectangle::count << " rectangles." << endl;
+    Rectangle r3 = addRectangle(r1,r2);
     cout << "In total there are " << Rectangle::count << " rectangles." << endl;
 
     return 0;
