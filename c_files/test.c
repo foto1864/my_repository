a425da3a6c02ff3c59991a55456c1666d081ef9d
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-17 20:37:34 +0200

    date info added

--- c_files/test.c
+++ c_files/test.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define MAX_NAME_LENGTH 50
 
 typedef struct {
     char name[MAX_NAME_LENGTH];
-    int date;   // Date 
-    int month;  // Month
-    int year;   // Year
+    int date;     // Date 
+    int month;    // Month
+    int year;     // Year
+    int hours;    // Hours
+    int minutes;  // Minutes
+
 } info;
 
 typedef info* Info;
 
-Info get_information(FILE *);
-void print_information(Info);
+Info get_date_info(void);
+void print_date_info(Info);
+Info get_bday_information(FILE*);
+void print_bday_information(Info);
 
 int main(void) {
-    FILE *file;
+    
+    Info today = get_date_info();
+    print_date_info(today);
+
+    FILE *file; 
     file = fopen("birthdays.txt", "r");
     if (file == NULL) {
         fprintf(stderr, "Could not open file!");
@@ -25,22 +35,26 @@ int main(void) {
     int num_of_people = getc(file)-48;
     Info *info_array = malloc(num_of_people * sizeof(Info));
     for (int i=0; i<num_of_people; i++) {
-        info_array[i] = get_information(file);
+        info_array[i] = get_bday_information(file);
     }
     for (int i=0; i<num_of_people; i++) {
-        print_information(info_array[i]);
+        print_bday_information(info_array[i]);
     }
+
     return 0;
 }
 
-// Expected User Input is DD MM YYYY Name Name\n
-
-Info get_information(FILE *read_file) {
+// Expected User Input is DD MM YYYY Name Name\n in each line.
+Info get_bday_information(FILE *read_file) {
 
     Info info = malloc(sizeof(*info));
     fscanf(read_file, "%d", &info->date);       // Get the day
     fscanf(read_file, "%d", &info->month);      // Get the month
     fscanf(read_file, "%d", &info->year);       // Get the year
+    
+    // Will not be needed but good to have been initialized.
+    info->hours = 0;        
+    info->minutes = 0;      
 
     // Get the name:
     // We first store the name in a temporary buffer because we are
@@ -63,8 +77,32 @@ Info get_information(FILE *read_file) {
     return info;
 }
 
-void print_information(Info info) {
+void print_bday_information(Info info) {
     printf("The name of the peasant is %s ", info->name);
     printf("and his birthday is on: %02d/%02d/%d.\n", info->date, info->month, info->year);
+    return;
+}
+
+// Gets the information of the current date
+Info get_date_info(void) {
+    time_t t = time(NULL);
+    struct tm day = *localtime(&t);
+    Info info = malloc(sizeof(*info));
+
+    info->date = day.tm_mday;                  // Get the date
+    info->month = day.tm_mon + 1;              // Get the month [Counting is 0 to 11]
+    info->year = day.tm_year + 1900;           // Get the year [Years since 1900]
+    info->hours = day.tm_hour;                 // Get the hours
+    info->minutes = day.tm_min;                // Get the minutes
+    
+    return info;
+}
+
+// Prints the information of the current date
+void print_date_info(Info info) {
+    // Print The Current Date
+    printf("Current date is %02d/%02d/%d\n", info->date, info->month, info->year);
+    // Print The Current Time
+    printf("Current time is %02d:%02d\n", info->hours, info->minutes);
     return;
 }
