c8a386c615e6c2c9d15bab51f1d9e634ff7ebb27
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-22 12:18:04 +0200

    Maybe I should, POP THE CHAIN FOR THIS ONE

--- cpp_files/test_stl_lib/test_lib.cpp
+++ cpp_files/test_stl_lib/test_lib.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+void print(int);
+
 int main(void) {
-    cout << "C++ is trash when compared to C fam." << endl;
+
+    vector<int> v;
+    for (int i=0; i<10; i++) {
+        v.push_back(0);
+    }
+    cout << "Size of vector is " << v.size() << endl;
+
+    for (int i=0; i<5; i++) {
+        v.pop_back();
+    }
+
+    cout << "After popping the chain the size of the vector is " << v.size() << endl;
+
+    vector<int>::iterator Iter;
+    for_each(v.begin(), v.end(), print);
+
     return 0;
+}
+
+void print(int n) {
+    cout << n << endl;
 }
