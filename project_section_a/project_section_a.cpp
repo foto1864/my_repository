5177bde74b61e3b8b03b98128539b37df91b37a9
foto1864 <fotinakisgiannis23@gmail.com>
2023-11-27 14:45:25 +0200

    changed makefile and started secretary over again

--- project_section_a/project_section_a.cpp
+++ project_section_a/project_section_a.cpp
@@ -1,106 +1,57 @@
 #include <iostream>
 #include <cstdlib>
-#define MIN_UNIVERSITY_SIZE 10
-using namespace std;    // Dox aint coming
+using namespace std;    
 
 class Person {
     private:
         string name;
         string email_address;
-        int64_t phone_number;
+        string phone_number;
         uint birth_year;
         uint academic_ID;
     public:
         static int count;
         // Mutator Functions
-        void set_name(string n);
-        void set_email_address(string email);
-        void set_birth_year(uint year);
-        void set_phone_number(uint number);
-        void set_academic_ID(uint id);
+        void set_name(string);
+        void set_email_address(string );
+        void set_phone_number(string);
+        void set_birth_year(uint);
+        void set_academic_ID(uint);
         // Accessor Functions
         string get_name(void);
         string get_email_address(void);
-        int64_t get_phone_number(void);
+        string get_phone_number(void);
         uint get_birth_year(void);
         uint get_academic_ID(void);
         // Operator Overloading
-        friend ostream &operator<<(ostream &str, Person &person);
-        friend istream &operator>>(istream &str, Person &person);
+        friend ostream &operator<<(ostream &, Person &);
+        friend istream &operator>>(istream &, Person &);
         // Constructors And Destructor
         Person();
         Person(string, uint);
-        Person(string, string, int64_t, uint, uint);
+        Person(string, string, string, uint, uint);
         ~Person();
 };
 
 class Secretary {
     private:
-        Person **university;
-        uint university_capacity;
+
     public:
-        uint university_size;
-        void insert_person(Person *);
-        Person** extend_university(Person **, uint);
         Secretary();
+        void insert_person(Person*);
 };
 
 ////////////////////////////////// FUNCTIONS FOR CLASS SECRETARY //////////////////////////////////
 
 Secretary::Secretary() {
-    university = (Person**) malloc(MIN_UNIVERSITY_SIZE*sizeof(Person*));
-    for (int i=0; i<MIN_UNIVERSITY_SIZE; i++) {
-        university[i] = NULL;
-    }
-    university_capacity = MIN_UNIVERSITY_SIZE;
-    university_size = 0;
-}
-
-Person** Secretary::extend_university(Person **university, uint previous_capacity) {
-    Person **newUniversity = (Person**) malloc(2*previous_capacity*sizeof(Person*));
-    for (uint i=0; i<previous_capacity; i++) {
-        newUniversity[i] = university[i];
-    }
-    for (uint i=0; i<previous_capacity; i++) {
-        newUniversity[previous_capacity+i] = NULL;
-    }
-    free(university);
-    return newUniversity;
 }
 
 void Secretary::insert_person(Person *p) {
-    int pos = 0;
-    int stop_loop;
-    if (university_size > MIN_UNIVERSITY_SIZE)
-        stop_loop = university_size;
-    else 
-        stop_loop = MIN_UNIVERSITY_SIZE;
-
-    for (pos=0; pos < stop_loop; pos++) {
-        if (university[pos] == NULL) break;
-    }
-    // This means that the person is going to be inserted at index pos
-    university[pos] = p;
-    university_size++;
-    if (university_size * 2 > university_capacity) {
-        university = extend_university(university, university_capacity);
-        cout << "Resize happened!" << endl;
-    }
 }
-
-// Do I need to use a given data structure or create my own one?
-// Why does the second option seem easier bruv?
-
-// There is no segfault in creating my own vector
-// Because Im him
-
-// I Know Victorias Secret as they say
-
-
 
 int main(void) {
 
-    Person p1("Giannis", "foto@gmail.com", 6972521094, 2004, 2200207);
+    Person p1("Giannis", "foto@gmail.com", "6972521094", 2004, 2200207);
     cout << p1;
 
     Secretary university;
@@ -125,15 +76,15 @@ int Person::count = 0;
 // Mutator Functions
 void Person::set_name(string n) { name = n; return; }
 void Person::set_email_address(string email) { email_address = email; return; }
+void Person::set_phone_number(string phone_num) { phone_number = phone_num; return; }
 void Person::set_birth_year(uint year) { birth_year = year; return; }
-void Person::set_phone_number(uint number) { phone_number = number; return; }
 void Person::set_academic_ID(uint id) { academic_ID = id; return; }
 
 // Accessor Functions
 string Person::get_name(void) { return name; }
 string Person::get_email_address(void) { return email_address; }
+string Person::get_phone_number(void) { return phone_number; }
 uint Person::get_birth_year(void) { return birth_year; }
-int64_t Person::get_phone_number(void) { return phone_number; }
 uint Person::get_academic_ID(void) { return academic_ID; }
 
 // Operator Overloading
@@ -159,6 +110,6 @@ istream &operator>>(istream &str, Person &person) {
 // Constructors And Destructor
 Person::Person() { count++; } 
 Person::Person(string n, uint id) : name(n), academic_ID(id) { count++; }
-Person::Person(string n, string email, int64_t number, uint year, uint id) : 
+Person::Person(string n, string email, string number, uint year, uint id) : 
         name(n), email_address(email), phone_number(number), birth_year(year), academic_ID(id) { count++; }
 Person::~Person() { count--; }
