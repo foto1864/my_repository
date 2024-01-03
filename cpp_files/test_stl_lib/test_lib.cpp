ed378a95301e33fbf24c0d5f408aa2473453e6db
foto1864 <fotinakisgiannis23@gmail.com>
2024-01-03 17:59:50 +0200

    esu tha me xwriseis apo ti mana mou

--- cpp_files/test_stl_lib/test_lib.cpp
+++ cpp_files/test_stl_lib/test_lib.cpp
@@ -2,26 +2,6 @@
 #include <cstdlib>
 using namespace std;
 
-typedef enum {
-    FIRST_SEM = 1,
-    SECOND_SEM = 2,
-    THIRD_SEM = 3,
-    FOURTH_SEM = 4,
-    FIFTH_SEM = 5,
-    SIXTH_SEM = 6,
-    SEVENTH_SEM = 7,
-    EIGHTH_SEM = 8,
-    NINTH_SEM = 9,
-    TENTH_SEM = 10,
-    ELEVENTH_SEM = 11,
-    TWELVETH_SEM = 12,
-} Semester;
-
 int main(void) {
-    Semester sem = FIRST_SEM;
-    int semes = 5;
-    cout << sizeof(semes) << endl;
-    cout << sizeof(sem) << endl;
-    cout << sem << endl;
     return 0;
 }
