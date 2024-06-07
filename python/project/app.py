9564ad25df3f48d03c3d46a54d94f40f6a7372c5
foto1864 <fotinakisgiannis23@gmail.com>
2024-06-07 19:17:58 +0300

    μαζι τα φαγαμε

--- python/project/app.py
+++ python/project/app.py
@@ -214,3 +214,43 @@ def traceActorInfluence(actorId):
 # sort  by the 3rd element of a tuple (movies of actor)
 def sort(tuple):
     return tuple[2]
+
+
+def colleaguesOfColleagues(actorId1, actorId2):
+    # Create a new connection
+    con = connection()
+
+    # Create a cursor on the connection
+    cur = con.cursor()
+
+    sql = '''SELECT m.title, r1.actor_id AS colleagueOfActor1, r2.actor_id AS colleagueOfActor2, %s AS actor1, %s AS actor2
+            FROM role r1, role r2, movie m
+            WHERE r1.movie_id = r2.movie_id
+            AND m.movie_id = r1.movie_id
+            AND r1.actor_id != r2.actor_id
+            AND r1.actor_id IN (
+                SELECT r4.actor_id
+                FROM role r4
+                WHERE r4.movie_id IN (
+                    SELECT movie_id FROM role WHERE actor_id = %s
+                )
+            )
+            AND r2.actor_id IN (
+                SELECT r5.actor_id
+                FROM role r5
+                WHERE r5.movie_id IN (
+                    SELECT movie_id FROM role WHERE actor_id = %s
+                )
+            )'''
+
+    cur.execute(sql, (actorId1, actorId2, actorId1, actorId2))
+    res = cur.fetchall()
+
+    # Convert the result into the desired format
+    results = [(row[0], row[1], row[2], row[3], row[4]) for row in res]
+
+    # Close the cursor and the connection
+    cur.close()
+    con.close()
+
+    return results
