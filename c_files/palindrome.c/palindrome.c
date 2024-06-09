5e8fc63380981e55075e6576fa024716a8ee4205
foto1864 <fotinakisgiannis23@gmail.com>
2024-06-09 23:52:41 +0300

    Face

--- c_files/palindrome.c/palindrome.c
+++ c_files/palindrome.c/palindrome.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 // Συνάρτηση για να βρει το μέγιστο μήκος μιας παλινδρομικής υποσυμβολοσειράς και την αρχική της θέση
-void longest_palindromic_substring(char* s, int* max_length, int* start_index) {
+int longest_palindromic_substring(char* s, int* max_length, int* start_index) {
     int n = strlen(s);
     if (n == 0) {
-        *max_length = 0;
-        *start_index = -1;
-        return;
+        return -1;
     }
 
+    // Δημιουργία και αρχικοποίηση του πίνακα dp
     bool dp[n][n];
-    memset(dp, 0, sizeof(dp));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = false;
+        }
+    }
 
     *max_length = 1;
     *start_index = 0;
@@ -22,17 +26,8 @@ void longest_palindromic_substring(char* s, int* max_length, int* start_index) {
         dp[i][i] = true;
     }
 
-    // Έλεγχος για συμβολοσειρές μήκους 2
-    for (int i = 0; i < n - 1; i++) {
-        if (s[i] == s[i + 1]) {
-            dp[i][i + 1] = true;
-            *start_index = i;
-            *max_length = 2;
-        }
-    }
-
     // Έλεγχος για μεγαλύτερα μήκη
-    for (int length = 3; length <= n; length++) {
+    for (int length = 2; length <= n; length++) {
         for (int i = 0; i < n - length + 1; i++) {
             int j = i + length - 1;
             if (s[i] == s[j] && dp[i + 1][j - 1]) {
@@ -42,14 +37,19 @@ void longest_palindromic_substring(char* s, int* max_length, int* start_index) {
             }
         }
     }
+    return 0;
 }
 
 int main() {
-    char s[] = "nisonanomimatamimonanosin";
+    char s[] = "FaceFabdFabddecaF";
     int max_length;
     int start_index;
 
-    longest_palindromic_substring(s, &max_length, &start_index);
+    int returned = longest_palindromic_substring(s, &max_length, &start_index);
+    if (returned < 0) {
+        fprintf(stderr, "Fuck You.\n");
+        exit(returned);
+    }
 
     printf("Length of original string: %ld\n", strlen(s));
     printf("Max length of palindrome substring: %d\n", max_length);
