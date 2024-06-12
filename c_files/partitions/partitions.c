03a38071202d38eb367c422dfb85acb3eb5f5d67
foto1864 <fotinakisgiannis23@gmail.com>
2024-06-13 00:57:12 +0300

    customized it

--- c_files/partitions/partitions.c
+++ c_files/partitions/partitions.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-bool canPartition(int* nums, int numsSize) {
+bool can_be_split(int* nums, int numsSize) {
     int total_sum = 0;
     for (int i = 0; i < numsSize; i++) {
         total_sum += nums[i];
     }
-
-    // Αν το συνολικό άθροισμα είναι περιττό, δεν μπορούμε να το χωρίσουμε σε δύο ίσα μέρη
+    // if total sum is odd, then it can't be split in half (optimization)
     if (total_sum % 2 != 0) {
         return false;
     }
-
     int target = total_sum / 2;
-
-    // Δημιουργούμε πίνακα δυναμικού προγραμματισμού
+    // create dynamic programming array
     bool dp[target + 1];
     for (int i = 0; i <= target; i++) {
         dp[i] = false;
     }
     dp[0] = true;
-
-    // Συμπλήρωση του πίνακα δυναμικού προγραμματισμού
+    // fill the array
     for (int i = 0; i < numsSize; i++) {
         for (int j = target; j >= nums[i]; j--) {
             dp[j] = dp[j] || dp[j - nums[i]];
         }
     }
-
+    // return true if dp[target] > 0.
     return dp[target];
 }
 
 int main() {
-    int A[] = {1, 5, 12, 5};
-    int size = sizeof(A) / sizeof(A[0]);
-
-    if (canPartition(A, size)) {
-        printf("Ναι, μπορούμε να χωρίσουμε το σύνολο σε δύο υποσύνολα με ίσο άθροισμα.\n");
-    } else {
-        printf("Όχι, δεν μπορούμε να χωρίσουμε το σύνολο σε δύο υποσύνολα με ίσο άθροισμα.\n");
+    int* A;
+    int get_size;
+    printf("Give array size:\n");
+    scanf("%d", &get_size);
+    printf("Enter %d numbers that will go into the array:\n", get_size);
+    A = malloc(get_size*sizeof(int));
+    for(int i=0; i<get_size; i++) {
+        scanf("%d", &A[i]);
     }
-
+    if (can_be_split(A, get_size)) {
+        printf("Set can be split.\n");
+    } else {
+        printf("Set can't be split.\n");
+    }
+    free(A);
     return 0;
 }
