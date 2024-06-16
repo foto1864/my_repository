884a0ef456b6cd467414c348e451ac8feeb3f994
foto1864 <fotinakisgiannis23@gmail.com>
2024-06-16 21:01:21 +0300

    what a trash language fam wow

--- python/test/python.py
+++ python/test/python.py
@@ -1,11 +1,27 @@
-# First python program
+# Get ready bitches Im writing in python
 
-def add(a,b):
-    return a+b
+import math
 
-a = 5
-b = 6
+a = input("Give a number for a: ")
+b = input("Give a number for b: ")
+c = input("Give a number for c: ")
 
-c = add(a,b)
+if a == 0:
+    print("That ain't no quadradic fam we playin?")
+    exit
 
-print(c)
+print("Your quadradic is: "+a+"(x^2)+"+b+"x+"+c)
+
+delta = int(b)*int(b) - 4*int(a)*int(c)
+
+print(delta)
+
+if delta > 0:
+    x1 = (-int(b) + math.sqrt(delta)) / 2 * int(a)
+    x2 = (-int(b) - math.sqrt(delta)) / 2 * int(a)
+    print("Solutions are: "+str(x1)+" and "+str(x2))
+elif delta == 0:
+    x = (-int(b) + math.sqrt(delta)) / 2 * int(a)
+    print("Solution is: "+str(x))
+else :
+    print("There are no solutions to your quadradic.")
