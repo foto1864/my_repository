1c797cd13f634169d57f47a82e059e0176e37eb0
foto1864 <fotinakisgiannis23@gmail.com>
2024-06-21 21:30:28 +0300

    DEN EXO MATHI PAITHON STO SHOLIO

---
+++ python/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    int max_int = max(a, b);
+    cout << max_int << endl;
+    cout << "Hello world" << endl;
+    return 0;
+}
---
+++ python/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello world")
+a = input()
+b = input()
+c = input()
+
+if c == '+':
+    print(int(a)+int(b))
+elif c == '-':
+    print(int(a)-int(b))
