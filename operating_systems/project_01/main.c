2239b5435b6f30af99c1ee4e3299bc3f2abe09e6
foto1864 <fotinakisgiannis23@gmail.com>
2024-10-22 12:21:04 +0300

    valgrind installed and program runs

--- operating_systems/project_01/main.c
+++ operating_systems/project_01/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int money_value;
+    struct Node* next;
+} Node;
+
+Node* create_node(int value, Node* next) {
+    Node* new_node = malloc(sizeof(*new_node));
+    new_node->money_value = value;
+    new_node->next = next;
+    return new_node;
+}
+
+void print_node(Node* node) {
+    printf("The money value of the node is %d\n", node->money_value);
+    return;
+}
 
 int main() {
-    printf("We are so fucked\n");
+
+    Node* node = create_node(10, NULL);
+    print_node(node);
+    free(node);
+    
     return 0;
 }
