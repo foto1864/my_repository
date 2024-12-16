6bff58a113d5e7328e66dbbf917ab6737801632e
foto1864 <fotinakisgiannis23@gmail.com>
2024-12-16 12:49:55 +0200

    will check again

--- c_files/thema_5/main.c
+++ c_files/thema_5/main.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#define SIZE 8
 
 int main(void) {
-    fprintf(stdout, "Makefile works\n");
-    return 0;
-}
 
+    // Init Arrays
+    int array_A[SIZE] = {2,6,6,7,7,10,9,9};
+    int array_B[SIZE] = {3,6,7,8,8,9,9,10};
+
+    //int f_x = 0;
+    int sum_f_x = 0;
+
+    int start_interval = -10;
+    int end_interval = 10;
+
+    // for (int x = start_interval; x < end_interval; x++) {
+    //     for (int i = 0; i < SIZE; i++) {
+    //         if (x > array_B[i]) {
+    //             f_x = array_A[i] - x;
+    //         }   
+    //         else {
+    //             f_x = 0;
+    //         }
+    //         //printf("f(%d) = %d\n", x, f_x);
+    //         sum_f_x += f_x;
+    //     }
+    //     printf("F(%d) = %d\n", x,  sum_f_x);
+    // }
+    // return 0;
+
+    int x = 2;
+    
+    int f_x[SIZE];
+    for (int i=0; i<SIZE; i++) {
+        f_x[i] = 0;
+    }
+
+    for (int i=0; i<SIZE; i++) {
+        if (x > array_B[i]) {
+            f_x[i] = array_A[i] - x;
+        }
+        else {
+            f_x[i] = 0;
+        }
+    }
+
+    for (int i=0; i<SIZE; i++) {
+        sum_f_x += f_x[i];
+    }
+
+    printf("F(%d) = %d\n", x, sum_f_x);
+
+}
