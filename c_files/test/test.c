bd516f6bb2f347d8657f99bc452fdda46430ce76
foto1864 <fotinakisgiannis23@gmail.com>
2025-04-05 22:14:43 +0300

    when did i actually commit this?

---
+++ c_files/test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// This is a simple C program that prints "Hello, World!" to the console.
+// It includes the standard input-output library and defines the main function. 
